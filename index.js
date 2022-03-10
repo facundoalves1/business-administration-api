aaf4a9bc54b1a956eb8a4c54cf920aea34a40e9f
facundoalves1 <facundoalves1@gmail.com>
2022-03-10 07:08:40 -0300

    commit to secure changes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const express = require('express');+
+const express = require('express');
+const app = express();
 
-//new changes
+//Importing Routes
+const authRoute = require('./routes/auth');
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+
+
+app.listen(3000, ()=> console.log('Server Runnning...'));
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+router.post('/register',(req,res)=>{
+
+    res.send('Registered...');
+    
+});
+
+
+module.exports = router;
